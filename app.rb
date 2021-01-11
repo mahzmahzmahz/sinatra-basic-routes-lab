5bdde5a8e30a718df3d8572cca9d43f90c10979a
Alex Marz <alexhmarz@gmail.com>
2021-01-10 19:11:10 -0500

    Hot Toddy

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex"
+    end
+
+    get '/hometown' do  
+        "My hometown is Lexington, Massachusetts"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Neon' by John Mayer off the 'Where the Light Is' live album."
+    end
+
+
 end
